59c5ab7809a753b4c44a9ca981fac300c288a60a
Ze'ev Klapow <zklapow@gmail.com>
2018-01-25 16:54:09 -0500

    Delete scripts (#4)
    * Delete pull.sh
    
    * Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $(dirname "$0")
-HOST=dirty-hippo.iad03.hubspot-networks.net
-mvn -nsu clean package
-#rm -v out/*
-cd ../
-rsync -avz $HOST:~/SeatSolver/out/ ./SeatSolver/out/ &>/dev/null
-rsync -avz SeatSolver/ $HOST:~/SeatSolver/
-#/usr/bin/scp SeatSolver/data/data.json $HOST:~/SeatSolver/data/
